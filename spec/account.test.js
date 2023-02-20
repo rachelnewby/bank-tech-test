6212976cb9677a973e7e8791040c1c2011a993db
Rachel Newby <rachelcnewby@gmail.com>
2023-02-20 17:57:54 +0000

    Tests refactored to include beforeEach

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,14 +1,15 @@
 const Account = require('../lib/account');
 
 describe("Account", () => {
+  beforeEach(() => {
+    account = new Account;
+  });
 
   it("when no money has been deposited or withdrawn, initially", () => {
-    const account = new Account();
     expect(account.getAccountBalance()).toBe(0)
   });
 
   it("when no money has been deposited or withdrawn, initially activity is []", () => {
-    const account = new Account();
     expect(account.getAccountActivity()).toEqual([])
   });
 })

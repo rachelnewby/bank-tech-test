b768ab851a0fc18e89b161ecda02604086d7fe2f
Rachel Newby <rachelcnewby@gmail.com>
2023-02-21 15:11:18 +0000

    Statment unit test decimal places. Passed

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -24,6 +24,22 @@ describe("Statement", () => {
     expect(statement.print()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000.00 ||  || 1000.00")
   })
 
+  it("#print returns string with relevant information when decimal places have more than 2 numbers", () => {
+    const date = new Date();
+    date.setDate(10)
+    date.setMonth(0)
+    let accountDouble = { getAccountActivity: () => [
+      {
+        date: date,
+        credit: 1000.0,
+        debit: "",
+        balance: 1000.0
+      }
+    ] }
+    const statement = new Statement(accountDouble);
+    expect(statement.print()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000.00 ||  || 1000.00")
+  })
+
   it("#print returns string with relevant information", () => {
     const date = new Date();
     date.setDate(11)
@@ -40,7 +56,7 @@ describe("Statement", () => {
     expect(statement.print()).toEqual("date || credit || debit || balance\n11/02/2023 || 1000.82 ||  || 1000.82")
   })
 
-  it("#print returns string with relevant information", () => {
+  it("#print returns string with relevant information, checks month formatter", () => {
     const date = new Date();
     date.setDate(11)
     date.setMonth(10)
@@ -110,5 +126,37 @@ describe("Statement", () => {
     ] }
     const statement = new Statement(accountDouble);
     expect(statement.print()).toEqual("date || credit || debit || balance\n14/01/2023 ||  || 500.00 || 2500.00\n13/01/2023 || 2000.00 ||  || 3000.00\n10/01/2023 || 1000.00 ||  || 1000.00")
+  })
+
+  it("#print returns string with relevant information when multiple transactions have been made", () => {
+    const date = new Date();
+    date.setDate(10);
+    date.setMonth(0);
+    const dateTwo = new Date();
+    dateTwo.setDate(13);
+    dateTwo.setMonth(0);
+    const dateThree = new Date();
+    dateThree.setDate(14);
+    dateThree.setMonth(0);
+    let accountDouble = { getAccountActivity: () => [
+      {
+        date: date,
+        credit: 100.48284857,
+        debit: "",
+        balance: 100.48
+      }, {
+        date: dateTwo,
+        credit: 300.3223843,
+        debit: "",
+        balance: 400.80
+      }, {
+        date: dateThree,
+        credit: "",
+        debit: 200,
+        balance: 200.80000
+      }
+    ] }
+    const statement = new Statement(accountDouble);
+    expect(statement.print()).toEqual("date || credit || debit || balance\n14/01/2023 ||  || 200.00 || 200.80\n13/01/2023 || 300.32 ||  || 400.80\n10/01/2023 || 100.48 ||  || 100.48")
   })
 })

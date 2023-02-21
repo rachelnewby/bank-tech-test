db34099fa60a20ee89f20c378531712ba2d3950b
Rachel Newby <rachelcnewby@gmail.com>
2023-02-21 14:36:21 +0000

    Statement unit test: multiple transactions made. Passed

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -8,7 +8,7 @@ describe("Statement", () => {
     expect(statement.print()).toEqual("No transactions to display")
   })
 
-  it("initially, without any account information", () => {
+  it("#print returns string with relevant information", () => {
     const date = new Date();
     date.setDate(10)
     date.setMonth(0)
@@ -20,8 +20,31 @@ describe("Statement", () => {
         balance: 1000
       }
     ] }
-
     const statement = new Statement(accountDouble);
     expect(statement.print()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000.00 ||  || 1000.00")
+  })
+
+  it("#print returns string with relevant information when multiple transactions have been made", () => {
+    const date = new Date();
+    date.setDate(10);
+    date.setMonth(0);
+    const dateTwo = new Date();
+    dateTwo.setDate(13);
+    dateTwo.setMonth(0);
+    let accountDouble = { getAccountActivity: () => [
+      {
+        date: date,
+        credit: 1000,
+        debit: "",
+        balance: 1000
+      }, {
+        date: dateTwo,
+        credit: 2000,
+        debit: "",
+        balance: 3000
+      }
+    ] }
+    const statement = new Statement(accountDouble);
+    expect(statement.print()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000.00 ||  || 1000.00\n13/01/2023 || 2000.00 ||  || 3000.00")
   })
 })

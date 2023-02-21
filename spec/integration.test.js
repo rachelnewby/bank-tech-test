7a80d9faa12e4a06826a50d9b1fc4b7088430fea
Rachel Newby <rachelcnewby@gmail.com>
2023-02-21 11:22:00 +0000

    Integration test refactored

--- spec/integration.test.js
+++ spec/integration.test.js
@@ -2,30 +2,27 @@ const Account = require("../lib/account")
 const Statement = require('../lib/statement')
 
 describe("Banking app", () => {
+  beforeEach(() => {
+    account = new Account;
+    statement = new Statement(account);
+  })
+
   it("initially, it prints a message to say no transactions to show", () => {
-    const account = new Account;
-    const statement = new Statement(account);
     expect(statement.print()).toEqual("No transactions to display")
   })
 
   it("prints a statment with the deposit", () => {
-    const account = new Account;
-    const statement = new Statement(account);
     account.deposit(100);
     expect(statement.print()).toEqual("date || credit || debit || balance\n21/02/2023 || 100.00 ||  || 100.00")
   })
 
   it("prints a statment with the deposit and withdrawal", () => {
-    const account = new Account;
-    const statement = new Statement(account);
     account.deposit(100);
     account.withdraw(100)
     expect(statement.print()).toEqual("date || credit || debit || balance\n21/02/2023 || 100.00 ||  || 100.00\n21/02/2023 ||  || 100.00 || 0.00")
   })
 
   it("prints a statment with multiple deposits and withdrawals", () => {
-    const account = new Account;
-    const statement = new Statement(account);
     account.deposit(100);
     account.withdraw(100);
     account.deposit(300);

87e619c73257d5a6b01b1bb07b4eda396a1512c5
Rachel Newby <rachelcnewby@gmail.com>
2023-02-21 14:31:39 +0000

    Statement unit test; #print returns correct string passed

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -7,4 +7,21 @@ describe("Statement", () => {
     const statement = new Statement(accountDouble);
     expect(statement.print()).toEqual("No transactions to display")
   })
+
+  it("initially, without any account information", () => {
+    const date = new Date();
+    date.setDate(10)
+    date.setMonth(0)
+    let accountDouble = { getAccountActivity: () => [
+      {
+        date: date,
+        credit: 1000,
+        debit: "",
+        balance: 1000
+      }
+    ] }
+
+    const statement = new Statement(accountDouble);
+    expect(statement.print()).toEqual("date || credit || debit || balance\n10/01/2023 || 1000.00 ||  || 1000.00")
+  })
 })
